02c13cb95a176d04e73723333953fa15be994f7d
lizka <lizka4231@gmail.com>
2025-10-13 23:44:28 +0300

    feat(client): add HomePage and routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,13 @@
-function App() {
-   return <>ui crime screne</>;
-}
+import { type FC } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+
+const App: FC = () => {
+   return (
+      <Routes>
+         <Route path="/" element={<HomePage />} />
+      </Routes>
+   );
+};
 
 export default App;
