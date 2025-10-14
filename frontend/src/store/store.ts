1e8e772529b22f3983176ac885abebb906c21de2
Liza <lizka4231@gmail.com>
2025-10-14 17:04:14 +0300

    feat(client): add appDispatch and appSelector hooks for types

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,8 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
-import ModalSlice from "./modalSlice";
+import modalReducer from "./modalSlice";
+import { useDispatch, useSelector, type TypedUseSelectorHook } from "react-redux";
 
 export const store = configureStore({
    reducer: {
-      modal: ModalSlice
+      modal: modalReducer
    }
 })
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
