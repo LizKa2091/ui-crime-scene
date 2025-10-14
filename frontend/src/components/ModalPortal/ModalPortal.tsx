a58a906691bbf8a06a1bddfdd371b0ee8e16fd8b
Liza <lizka4231@gmail.com>
2025-10-14 17:15:22 +0300

    feat(ModalPortal): implement redux into ModalPortal

--- frontend/src/components/ModalPortal/ModalPortal.tsx
+++ frontend/src/components/ModalPortal/ModalPortal.tsx
@@ -1,31 +1,46 @@
 import { useEffect, useState, type FC } from 'react';
 import { createPortal } from 'react-dom';
+import { useDispatch } from 'react-redux';
+import { hideModal } from '../../store/modalSlice';
+import { type modalType } from '../../types/modalTypes';
+import { useAppSelector } from '../../store/store';
 
-interface IModalPortalProps {
-   modalType: null | 'completeScene' | 'hint' | 'about';
-   onClose: () => void;
-}
-
-const ModalPortal: FC<IModalPortalProps> = ({ isShowing, onClose }) => {
+const ModalPortal: FC = () => {
    const [portalEl, setPortalEl] = useState<HTMLElement | null>(null);
+   const dispatch = useDispatch();
 
    useEffect(() => {
       const aside = document.getElementById('modal-result');
       setPortalEl(aside);
 
       const handleClickOutside = (e: MouseEvent) => {
-         if (aside && aside.contains(e.target as Node)) onClose();
+         if (aside && aside.contains(e.target as Node)) dispatch(hideModal());
       }
 
-      document.addEventListener('mousedown', handleClickOutside);
+      document.addEventListener('mousedown', handleClickOutside); 
 
       return () => document.removeEventListener('mousedown', handleClickOutside);
-   }, [onClose]);
+   }, [dispatch]);
 
-   if (!isShowing || !portalEl) return null;
+   const currModalType: modalType = useAppSelector((state) => state.modal.activeModal)
+
+   if (!currModalType || !portalEl) return null;
+
+   const displayMessage = () => {
+      switch (currModalType) {
+         case 'completeScene': 
+            return <p>Дело успешно раскрыто!</p>
+         case 'hint':
+            return <p>Подсказка</p>
+         case 'about':
+            return <p>Добро пожаловать в UI Crime Scene. Найди все UX-преступления!</p>
+         default: 
+            return null
+      }
+   }
 
    return (
-      createPortal(<p>Дело успешно раскрыто!</p>, portalEl)
+      createPortal(<div className='modal-portal'>{displayMessage()}</div>, portalEl)
    )
 }
 
