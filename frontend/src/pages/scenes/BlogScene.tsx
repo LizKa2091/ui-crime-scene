a6af34a1edba1346fc69a74c24acacdfaf4b6f70
lizka <lizka4231@gmail.com>
2025-10-15 23:57:31 +0300

    feat(pages scenes): implement store reducers into scenes

--- frontend/src/pages/scenes/BlogScene.tsx
+++ frontend/src/pages/scenes/BlogScene.tsx
@@ -1,7 +1,22 @@
 import { type FC } from 'react';
+import { Flex } from 'antd';
+import { useAppDispatch } from '../../store/store';
+import { markFoundError, setScene } from '../../store/crimeSlice';
 
 const BlogScene: FC = () => {
-   return <div>blog scene</div>;
+   const dispatch = useAppDispatch();
+   dispatch(setScene('blog'));
+
+   const handleErrorClick = () => {
+      dispatch(markFoundError({ errorId: 1 }));
+   };
+
+   return (
+      <Flex>
+         <h2>blog scene</h2>
+         <Flex onClick={handleErrorClick}>я ошибка, нажми на меня</Flex>
+      </Flex>
+   );
 };
 
 export default BlogScene;
