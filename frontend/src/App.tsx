9720f3ee48e88c1c4248682dddbb78d3e0444553
lizka <lizka4231@gmail.com>
2025-10-15 23:55:04 +0300

    feat(App): add MainLayout for routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,14 +4,43 @@ import HomePage from './pages/HomePage';
 import LoginScene from './pages/scenes/LoginScene';
 import ShopScene from './pages/scenes/ShopScene';
 import BlogScene from './pages/scenes/BlogScene';
+import MainLayout from './pages/MainLayout';
 
 const App: FC = () => {
    return (
       <Routes>
-         <Route path="/" element={<HomePage />} />
-         <Route path="/scene/login" element={<LoginScene />} />
-         <Route path="/scene/shop" element={<ShopScene />} />
-         <Route path="/scene/blog" element={<BlogScene />} />
+         <Route
+            path="/"
+            element={
+               <MainLayout>
+                  <HomePage />
+               </MainLayout>
+            }
+         />
+         <Route
+            path="/scene/login"
+            element={
+               <MainLayout>
+                  <LoginScene />
+               </MainLayout>
+            }
+         />
+         <Route
+            path="/scene/shop"
+            element={
+               <MainLayout>
+                  <ShopScene />
+               </MainLayout>
+            }
+         />
+         <Route
+            path="/scene/blog"
+            element={
+               <MainLayout>
+                  <BlogScene />
+               </MainLayout>
+            }
+         />
       </Routes>
    );
 };
