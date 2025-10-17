cb0d17e0f1e7e75034fd9a464b6fb00e12820082
Liza <lizka4231@gmail.com>
2025-10-17 13:11:46 +0300

    feat(modalSlice): add modalData field

--- frontend/src/store/modalSlice.ts
+++ frontend/src/store/modalSlice.ts
@@ -3,14 +3,17 @@ import { createSlice } from '@reduxjs/toolkit';
 const modalSlice = createSlice({
    name: 'modal',
    initialState: {
-      activeModal: null
+      activeModal: null,
+      modalData: null
    },
    reducers: {
       showModal: (state, action) => {
-         state.activeModal = action.payload
+         state.activeModal = action.payload;
+         state.modalData = action.payload.data ?? null;
       },
       hideModal: (state) => {
-         state.activeModal = null
+         state.activeModal = null;
+         state.modalData = null;
       }
    }
 })
