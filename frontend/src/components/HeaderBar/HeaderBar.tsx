72600b1e778a635b275817053e12f53ff4686033
lizka <lizka4231@gmail.com>
2025-10-18 14:36:45 +0300

    feat(HeaderBar): add layout

--- frontend/src/components/HeaderBar/HeaderBar.tsx
+++ frontend/src/components/HeaderBar/HeaderBar.tsx
@@ -1,10 +1,18 @@
-import { type FC } from 'react'
+import { type FC } from 'react';
+import { Link } from 'react-router-dom';
+import { Flex } from 'antd';
+import { Header } from 'antd/es/layout/layout';
+import styles from './HeaderBar.module.scss';
 
 const HeaderBar: FC = () => {
    return (
-      <div>
-         headerbar
-      </div>
+      <Header className={styles.header}>
+         <Flex align='center' justify='space-between' className={styles.headerContainer}>
+            <h1>UI Crime Scene</h1>
+            <p>Найдено: 2/4</p>
+            <Link to='/'>На главную</Link>
+         </Flex>
+      </Header>
    )
 }
 
