bedb18a2a4027239f6402e1f587596b6e596cf90
lizka <lizka4231@gmail.com>
2025-10-20 15:58:03 +0300

    feat(HomePage): update layout

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,8 +1,22 @@
 import { type FC } from 'react';
+import { Flex } from 'antd';
+
+import { useScenesProgress } from '../hooks/useScenesProgress';
+
+import SceneItem from '../components/SceneItem/SceneItem';
 
 const HomePage: FC = () => {
+   const scene = useScenesProgress();
+
    return (
-      <div>home</div>
+      <Flex justify='center' align='center'>
+         {scene && 
+            <Flex vertical align='center'>
+               <h2>Выберите сцену</h2>
+               <SceneItem title={scene.sceneName} progress={scene.status} />
+            </Flex>
+         }
+      </Flex>
    )
 };
 
