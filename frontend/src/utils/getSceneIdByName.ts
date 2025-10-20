216bdcec59359e8d8c8afdefa59d8da0b50cf88f
lizka <lizka4231@gmail.com>
2025-10-20 20:06:35 +0300

    feat(utils): add getSceneIdByName util

---
+++ frontend/src/utils/getSceneIdByName.ts
@@ -0,0 +1,9 @@
+const sceneIds: Record<string, number> = {
+   login: 1,
+   shop: 2,
+   blog: 3
+}
+
+export const getSceneIdByName = (sceneName: string): number | null => {
+   return sceneIds[sceneName] ?? null;
+}
